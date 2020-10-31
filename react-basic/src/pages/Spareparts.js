d931b9da4768426ac1082ec630ff247c7d20d450
sabsabila <58177825+sabsabila@users.noreply.github.com>
2020-10-31 20:36:20 +0700

    Fix search bar

--- react-basic/src/pages/Spareparts.js
+++ react-basic/src/pages/Spareparts.js
@@ -53,7 +53,7 @@ const Spareparts = () => {
           <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto benjol-brand">
           <Form inline expand="md" >
-            <Form.Control type="text" placeholder="Cari bengkel..." size="sm" className="mb-1 mr-sm-1 rounded-pill" />
+            <Form.Control style={{paddingRight: 270}} type="text" placeholder="Cari sparepart" size="sm" className="mb-1 mr-sm-1 rounded-pill" />
             <Button size="sm" className="button-custom fa fa-search rounded-pill mr-sm-5" type="submit">Search</Button>
           </Form>
               <Nav.Link href="/services" className="nav-link-custom">Services</Nav.Link>
